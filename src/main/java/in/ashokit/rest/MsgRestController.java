9f505dce6180894ea88279fb6d3b166315b7bcf8
Sahil <sahilansari567@gmail.com>
2024-08-11 18:58:21 +0530

    Update MsgRestController with @RequestMapping annotation

--- src/main/java/in/ashokit/rest/MsgRestController.java
+++ src/main/java/in/ashokit/rest/MsgRestController.java
@@ -2,18 +2,19 @@ package in.ashokit.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.ashokit.service.MsgService;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api")  // Keep this if you want all endpoints in this controller to start with /api
 public class MsgRestController {
 	
 	@Autowired
 	private MsgService msgService;
 	
-	@GetMapping("/welcome")
+	@GetMapping("/welcome")  // This maps the /api/welcome endpoint to this method
 	public String getMsg() {
 		int i = 10;
 		int j = 20;
